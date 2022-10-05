eeec27fc39a579f715d8c233a584b1d125133ebf
ryksyy <miko.prykari@gmail.com>
2022-10-06 02:10:20 +0300

    auto deploy testing 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> test1
+          Edit <code>src/App.js</code> hello world! this is a showcase
         </p>
         <a
           className="App-link"
